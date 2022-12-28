02f77475ac17561bacd659ef980c53290bd882ea
fatihalpozdemir <ozdemir.fatihalp@gmail.com>
2022-12-28 04:23:26 -0500

    commit try two

--- src/Task1/Number4.java
+++ src/Task1/Number4.java
@@ -3,5 +3,6 @@ package Task1;
 public class Number4 {
     public static void main(String[] args) {
         System.out.println(" This is for number 4 ");
+        System.out.println("123 ");
     }
 }
