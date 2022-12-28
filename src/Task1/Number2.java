da571da0473b3c5298fdc170381c5d9f86ef889d
fatihalpozdemir <ozdemir.fatihalp@gmail.com>
2022-12-28 02:39:08 -0500

    commit try two

--- src/Task1/Number2.java
+++ src/Task1/Number2.java
@@ -2,6 +2,7 @@ package Task1;
 
 public class Number2 {
     public static void main(String[] args) {
-        System.out.println("Green colour");
+        System.out.println("Green color");
+        System.out.println("blue color");
     }
 }
